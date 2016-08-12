8bcbc49a422b3f77957f71e2a7a40599c43ebfa1
Anders Tunold Kråkenes & Kiren Srinivasan <pair+anderskraken.tuneguru@devbootcamp.com>
2016-08-12 12:31:23 -0400

    Complete view and start on controller

--- controller/controller.rb
+++ controller/controller.rb
@@ -1,17 +1,36 @@
 
+require_relative "../view/view"
+require 'pry'
 
 class AkmeController
 
 # initialize new view and deck (our model)
+# deck = Deck.new
 
-view = View.new
-deck = Deck.new
+def initialize(ary)
+  @view = View.new
+  # @deck = Deck.new  # TBA
+  deck_array = @view.load_file(ary[0])
+  # run  # TBA
+end
+
 
 # get the file (from view)
-
-
-
 # call parser (from deck), (which will turn into into an array of card objects)
+
+# def run
+#   p "run works!"
+#   @deck.stuff
+#   @view.display.question(string)
+#   question_looper
+#   evaluate_all
+#   @view.display_final_result(correct_no, total_no)
+# end
+
+def question_looper
+
+end
+
 
 # loop through the arry in the deck:
   # call the display question (from view) to display upcoming question
